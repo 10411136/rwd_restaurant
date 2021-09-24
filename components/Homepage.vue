ee2f99d8a3c8fea95ad350658ebbf35da61a94c9
YuLin <10411136@gm.nttu.edu.tw>
2021-09-25 00:46:34 +0800

    finished

--- components/Homepage.vue
+++ components/Homepage.vue
@@ -29,7 +29,7 @@
           <div class="home__container bd-container bd-grid">
             <div class="home__data">
               <h1 class="home__title">Tasty food</h1>
-              <h2 class="home__subtitle">Try the best food of <br> hte week.</h2>
+              <h2 class="home__subtitle">Try the best food of <br> the week.</h2>
               <a href="#" class="button">view menu</a>
             </div>
             <img src="@/static/home.png" class="home__img" alt="HOMEIMG">
