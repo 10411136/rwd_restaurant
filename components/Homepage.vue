b65ed073608e395f435ecc940f1d4e005e927282
YuLin <10411136@gm.nttu.edu.tw>
2021-09-25 22:18:28 +0800

    修正Homepage

--- components/Homepage.vue
+++ components/Homepage.vue
@@ -1,104 +1,3 @@
-<script>
-export default {
-    data(){
-      return{
-        darkTheme: 'dark-theme',
-        iconTheme: 'bx-sun'
-      }
-    },
-    methods:{
-        /*==================== SHOW MENU ====================*/
-        showMenu(toggleId, navId){
-            // console.log('1=>',toggleId, navId);
-            const toggle = document.getElementById(toggleId),
-            nav = document.getElementById(navId);
-            // console.log('2=>',toggle, nav);
-            // Validate that variables exist
-            if(toggle && nav){
-                // We add the show-menu class to the div tag with the nav__menu class
-                nav.classList.toggle('show-menu');
-            }
-        },
-        linkAction(){
-            const navMenu = document.getElementById('nav-menu');
-            navMenu.classList.remove('show-menu');
-        },
-        scrollHeader(){
-            const nav = document.getElementById('header');
-            if(window.scrollY >= 200)
-                nav.classList.add('scroll-header');
-            else
-                nav.classList.remove('scroll-header');
-        },
-        scrollTop(){
-            const srollTop = document.getElementById('scroll-top');
-            if(window.scrollY >= 560)
-                srollTop.classList.add('scroll-top');
-            else
-                srollTop.classList.remove('scroll-top');
-        },
-        scrollActive(){
-            let sections = document.querySelectorAll('section[id]');
-            const scrollY = window.pageYOffset;
-
-            sections.forEach(current => {
-                const sectionHeight = current.offsetHeight;
-                const sectionTop = current.offsetTop - 50;
-
-                const sectionId = current.getAttribute('id');
-                if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-                }else{
-                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-                }
-            })
-        },
-        changeTheme(){
-          document.body.classList.toggle(this.darkTheme);
-          document.getElementById('theme-button').classList.toggle(this.iconTheme);
-          localStorage.setItem('selected-theme', this.getCurrentTheme());
-          localStorage.setItem('selected-icon', this.getCurrentIcon());
-        },
-        getCurrentTheme(){
-          return document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
-        },
-        getCurrentIcon(){
-          return document.getElementById('theme-button').classList.contains(this.iconTheme) ? 'bx-moon' : 'bx-sun';
-        }
-        
-    },
-    mounted(){
-        window.addEventListener('scroll', this.scrollTop);
-        window.addEventListener('scroll', this.scrollHeader);
-        window.addEventListener('scroll', this.scrollActive);
-
-        const selectedTheme = localStorage.getItem('selected-theme');
-        const selectedIcon = localStorage.getItem('selected-icon');
-
-        // const getCurrentTheme = () => document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
-        // const getCurrentIcon = () => document.getElementById('theme-button').classList.contains(this.iconTheme) ? 'bx-moon' : 'bx-sun';
-
-        if(selectedTheme){
-          document.body.classList[selectedTheme === 'dark'?'add':'remove'](this.darkTheme);
-          document.getElementById('theme-button').classList[selectedIcon === 'bx-moon'?'add':'remove'](this.iconTheme);
-        }
-        const sr = ScrollReveal({
-          origin: 'top',
-          distance: '30px',
-          duration: 2000,
-          reset: true
-        });
-        sr.reveal(`.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content`, {
-          interval: 200
-        });
-    },
-    destroyd () {
-        window.removeEventListener('scroll', this.scrollTop);
-        window.removeEventListener('scroll', this.scrollHeader);
-        window.removeEventListener('scroll', this.scrollActive);
-    }
-}
-</script>
 <template>
   <div>
       <a href="#" class="scrolltop" id="scroll-top">
@@ -538,6 +437,109 @@ export default {
       </footer>
   </div>
 </template>
+
+<script>
+export default {
+    data(){
+      return{
+        darkTheme: 'dark-theme',
+        iconTheme: 'bx-sun'
+      }
+    },
+    methods:{
+        /*==================== SHOW MENU ====================*/
+        showMenu(toggleId, navId){
+            // console.log('1=>',toggleId, navId);
+            const toggle = document.getElementById(toggleId),
+            nav = document.getElementById(navId);
+            // console.log('2=>',toggle, nav);
+            // Validate that variables exist
+            if(toggle && nav){
+                // We add the show-menu class to the div tag with the nav__menu class
+                nav.classList.toggle('show-menu');
+            }
+        },
+        linkAction(){
+            const navMenu = document.getElementById('nav-menu');
+            navMenu.classList.remove('show-menu');
+        },
+        scrollHeader(){
+            const nav = document.getElementById('header');
+            if(window.scrollY >= 200)
+                nav.classList.add('scroll-header');
+            else
+                nav.classList.remove('scroll-header');
+        },
+        scrollTop(){
+            const srollTop = document.getElementById('scroll-top');
+            if(window.scrollY >= 560)
+                srollTop.classList.add('scroll-top');
+            else
+                srollTop.classList.remove('scroll-top');
+        },
+        scrollActive(){
+            let sections = document.querySelectorAll('section[id]');
+            const scrollY = window.pageYOffset;
+
+            sections.forEach(current => {
+                const sectionHeight = current.offsetHeight;
+                const sectionTop = current.offsetTop - 50;
+
+                const sectionId = current.getAttribute('id');
+                if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+                }else{
+                    document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+                }
+            })
+        },
+        changeTheme(){
+          document.body.classList.toggle(this.darkTheme);
+          document.getElementById('theme-button').classList.toggle(this.iconTheme);
+          localStorage.setItem('selected-theme', this.getCurrentTheme());
+          localStorage.setItem('selected-icon', this.getCurrentIcon());
+        },
+        getCurrentTheme(){
+          return document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
+        },
+        getCurrentIcon(){
+          return document.getElementById('theme-button').classList.contains(this.iconTheme) ? 'bx-moon' : 'bx-sun';
+        }
+        
+    },
+    mounted(){
+        window.addEventListener('scroll', this.scrollTop);
+        window.addEventListener('scroll', this.scrollHeader);
+        window.addEventListener('scroll', this.scrollActive);
+
+        const selectedTheme = localStorage.getItem('selected-theme');
+        const selectedIcon = localStorage.getItem('selected-icon');
+
+        // const getCurrentTheme = () => document.body.classList.contains(this.darkTheme) ? 'dark' : 'light';
+        // const getCurrentIcon = () => document.getElementById('theme-button').classList.contains(this.iconTheme) ? 'bx-moon' : 'bx-sun';
+
+        if(selectedTheme){
+          document.body.classList[selectedTheme === 'dark'?'add':'remove'](this.darkTheme);
+          document.getElementById('theme-button').classList[selectedIcon === 'bx-moon'?'add':'remove'](this.iconTheme);
+        }
+        const sr = ScrollReveal({
+          origin: 'top',
+          distance: '30px',
+          duration: 2000,
+          reset: true
+        });
+        sr.reveal(`.home__data, .home__img, .about__data, .about__img, .services__content, .menu__content, .app__data, .app__img, .contact__data, .contact__button, .footer__content`, {
+          interval: 200
+        });
+    },
+    destroyd () {
+        window.removeEventListener('scroll', this.scrollTop);
+        window.removeEventListener('scroll', this.scrollHeader);
+        window.removeEventListener('scroll', this.scrollActive);
+    }
+}
+</script>
+
 <style>
 :root{
     --header-height: 3rem;
